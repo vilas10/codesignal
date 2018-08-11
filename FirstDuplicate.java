014b15ede1e93b8fe52decb9bfee36aeb435ba2c
Vilas <vineverlas10@gmail.com>
2018-08-11 15:06:28 -0500

    Create FirstDuplicate.java

---
+++ FirstDuplicate.java
@@ -0,0 +1,15 @@
+class FirstDuplicate {
+  int firstDuplicate(int[] a) {
+    Set<Integer> set = new HashSet<>();
+    
+    for (int i = 0; i < a.length; i++) {
+        if (set.contains(a[i])) {
+            return a[i];
+        } else {
+            set.add(a[i]);
+        }
+    }
+    
+    return -1;
+  }
+}
